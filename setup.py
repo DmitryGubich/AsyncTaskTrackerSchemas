f6177e648a0bb936d48c4d47f8c5b09df890ad3e
Dmitry Gubich <gubich97@gmail.com>
2023-08-16 19:35:54 +0200

    packaging

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     version="1.0",
     description="Schema registry for UberPopugInc",
     long_description_content_type="text/markdown",
-    license="Proprietary",
     url="https://github.com/DmitryGubich/UberPopugIncSchemas",
     python_requires=">=3.9",
     packages=find_packages(),
