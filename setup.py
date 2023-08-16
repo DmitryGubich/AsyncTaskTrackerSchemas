f88db5b449ec84f1280f308f193fa12425362430
Dmitry Gubich <gubich97@gmail.com>
2023-08-16 23:07:09 +0200

    packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+from os import path
+
 from setuptools import find_packages, setup
+
+current_dir = path.abspath(path.dirname(__file__))
 
 setup(
     name="uber-popug-events",
-    version="1.3",
-    description="Schema registry for UberPopugInc",
+    version="1.4",
+    description="Provides client that can make requests to Enett",
     long_description_content_type="text/markdown",
+    license="MIT",
     url="https://github.com/DmitryGubich/UberPopugIncSchemas",
     python_requires=">=3.9",
     packages=find_packages(),
