7a1b1cd930bcfd96f02be25e5a9a0b13086b08c5
Dmitry Gubich <gubich97@gmail.com>
2023-08-16 22:42:17 +0200

    packaging

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     name="uber-popug-events",
-    version="1.1",
+    version="1.2",
     description="Schema registry for UberPopugInc",
     long_description_content_type="text/markdown",
     url="https://github.com/DmitryGubich/UberPopugIncSchemas",
     python_requires=">=3.9",
+    packages=["uber-popug-events"],
+    install_requires=["jsonschema"],
+    license="MIT",
     platforms=["any"],
 )
