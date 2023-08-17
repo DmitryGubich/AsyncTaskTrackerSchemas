0cf49da6457dbccd5eb7d99ab916b21d536478ac
Dmitry Gubich <gubich97@gmail.com>
2023-08-17 14:39:29 +0200

    events for tracker

---
+++ src/uber_popug_schemas/events.py
@@ -0,0 +1,9 @@
+class Auth:
+    USER_CREATED = "Auth.UserCreated"
+    USER_UPDATED = "Auth.UserUpdated"
+    USER_DELETED = "Auth.UserDeleted"
+
+
+class Tracker:
+    TASK_ASSIGNED = "Tracker.TaskAssigned"
+    TASK_COMPLETED = "Tracker.TaskCompleted"
